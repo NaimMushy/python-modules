5dbab2b8a12a97f1e49d98141ac309c985845f2c
Iris Bady <ibady@z2r11p6.42lyon.fr>
2026-01-27 12:23:39 +0100

    went over mod00 before turning it in

--- mod00/ex7/ft_seed_inventory.py
+++ mod00/ex7/ft_seed_inventory.py
@@ -16,11 +16,13 @@ def ft_seed_inventory(
         The unit type of a seed (packets, grams or square meters).
     """
     if unit == "packets":
-        print(f"{seed_type.capitalize()} seeds: {quantity} packets available")
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
     elif unit == "grams":
-        print(f"{seed_type.capitalize()} seeds: {quantity} grams total")
-    else:
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
+    elif unit == "area":
         print(
             f"{seed_type.capitalize()} seeds: "
             f"covers {quantity} square meters"
         )
+    else:
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit}")
